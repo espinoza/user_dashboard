d59f93aa2bf9e2cec06f685dfc978aeeaf64a598
Jorge Espinoza <jorgeespinozaortega@gmail.com>
2021-04-02 23:48:49 -0300

    Add user login

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -1,4 +1,18 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
+from .models import User
+from .forms.access import LoginForm
+
 
 def index(request):
     return render(request, "index.html")
+
+
+def login(request):
+
+    if request.method == "GET":
+        login_form = LoginForm()
+
+    if request.method == "POST":
+        login_form = LoginForm(request.POST)
+
+    return render(request, "login.html", {"login_form": login_form})
